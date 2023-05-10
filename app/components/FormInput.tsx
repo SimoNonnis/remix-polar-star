a8dc91d3248588ccfbec2247e9a66a4f9f8707db
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-10 12:13:34 +0300

    Style input elements

--- app/components/FormInput.tsx
+++ app/components/FormInput.tsx
@@ -1,10 +1,10 @@
 export default function FormInput(props) {
   return (
-    <div>
+    <div className="text-left mb-6">
       <label htmlFor={props.id}>{props.text}</label>
       <input
         {...props}
-        className="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+        className="mt-1 block w-full border-2 border-orange-500 rounded-md bg-gray-100   focus:bg-white "
       />
     </div>
   );
