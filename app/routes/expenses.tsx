0d9db6537249680ce84a719253abfecaf13ea6e6
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-11 14:24:26 +0300

    Change icon on expanses

--- app/routes/expenses.tsx
+++ app/routes/expenses.tsx
@@ -30,23 +30,19 @@ export default function ExpensesLayoutPage() {
       <div className="flex justify-center items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-10  h-10 me-4"
+          fill="currentColor"
+          className="w-10 h-10 me-4"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-          />
+          <path d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
         </svg>
 
         <Title>Expenses</Title>
       </div>
 
-      <Button to="add">add expense</Button>
+      <div className="my-4 ">
+        <Button to="add">add expense</Button>
+      </div>
 
       <Outlet />
       <ExpensesList expenses={DUMMY_EXPENSES} />
