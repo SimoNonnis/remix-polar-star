09459357751763e9783a579f9e732ff9da3584bb
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-13 12:00:04 +0300

    Add link to raw expenses

--- app/routes/_authenticated.expenses.tsx
+++ app/routes/_authenticated.expenses.tsx
@@ -27,8 +27,28 @@ export default function ExpensesLayoutPage() {
         <Title>Expenses</Title>
       </div>
 
-      <div className="my-4 ">
-        <Button to="add">add expense</Button>
+      <div className="flex justify-center items-center">
+        <div className="m-4 ">
+          <Button to="add">add expense</Button>
+        </div>
+        <div className="m-4 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 inline-block me-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+            />
+          </svg>
+
+          <a href="/expenses/raw">Load raw data</a>
+        </div>
       </div>
 
       <Outlet />
