593febadc69c887244ed1fa43a54aec97e354efd
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-13 08:22:26 +0300

    Use searchParams on Auth

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -1,9 +1,17 @@
-import { Link } from "@remix-run/react";
+import { useSearchParams, Link } from "@remix-run/react";
 
 import FormInput from "~/components/FormInput";
 import Button from "~/components/Button";
 
 export default function AuthForm() {
+  const [searchParams] = useSearchParams();
+  const authMode = searchParams.get("mode");
+
+  const submitBtn = authMode === "register" ? "Register" : "Login";
+  const optionButton =
+    authMode === "register" ? "Log in with existing user" : "Create a new user";
+  const optionLink = authMode === "register" ? "?mode=login" : "?mode=register";
+
   return (
     <form method="post">
       <FormInput id="email" text="Email" type="email" name="email" required />
@@ -17,10 +25,10 @@ export default function AuthForm() {
         minLength={4}
       />
       <div className="text-left">
-        <Button>Register</Button>
+        <Button>{submitBtn}</Button>
         <span className="mx-4">or</span>
-        <Link to="/auth" className="underline">
-          Log in with existing user
+        <Link to={optionLink} className="underline">
+          {optionButton}
         </Link>
       </div>
     </form>
