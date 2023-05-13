95706984acd247abbc92e5d177f4b38fd20fe89e
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-13 07:43:32 +0300

    Add types to Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,12 @@
 import { Link } from "@remix-run/react";
+import type { ReactElement } from "react";
 
-export default function Button({ children, to }) {
+type Props = {
+  children: string | ReactElement;
+  to?: string;
+};
+
+export default function Button({ children, to }: Props) {
   const classes =
     "capitalize border-2 border-orange-500 bg-orange-500 hover:bg-transparent text-orange-100 hover:text-slate-700 rounded-md p-2 max-w-xs m-auto transition-colors duration-300";
 
