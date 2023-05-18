6607dc69309887e83fefb2e8d34ca312dfd48edc
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-18 11:48:39 +0300

    Tweak on text

--- app/routes/_authenticated.expenses.tsx
+++ app/routes/_authenticated.expenses.tsx
@@ -8,7 +8,7 @@ import ExpensesList from "~/components/ExpensesList";
 import DUMMY_EXPENSES from "~/data/dummy";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "Shared Expenses Page" }];
+  return [{ title: "My Expenses" }];
 };
 
 export default function ExpensesLayoutPage() {
@@ -24,7 +24,7 @@ export default function ExpensesLayoutPage() {
           <path d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
         </svg>
 
-        <Title>Expenses</Title>
+        <Title>My Expenses</Title>
       </div>
 
       <div className="flex justify-center items-center">
