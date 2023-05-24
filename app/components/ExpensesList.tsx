858af9535e97aaff0fac7c4d6ae53eec3c8bb119
Simon Nonnis <simon.nonnis@protonmail.com>
2023-05-24 10:43:11 +0300

    Add a fallback text when no expenses

--- app/components/ExpensesList.tsx
+++ app/components/ExpensesList.tsx
@@ -1,6 +1,15 @@
 import ExpenseListItem from "~/components/ExpenseListItem";
 
 function ExpensesList({ expenses }) {
+  if (expenses.length === 0) {
+    return (
+      <p className="my-6">
+        You don't have any expense yet. Start adding expenses today.
+      </p>
+    );
+  }
+  console.log(expenses.length);
+
   return (
     <ol>
       {expenses.map((expense) => (
