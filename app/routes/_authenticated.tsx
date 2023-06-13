954262370a510b4a687459225f23bb2349adb632
Simon Nonnis <simon.nonnis@protonmail.com>
2023-06-13 14:42:33 +0300

    Read user from session

--- app/routes/_authenticated.tsx
+++ app/routes/_authenticated.tsx
@@ -1,5 +1,13 @@
 import { Outlet } from "@remix-run/react";
+
+import { getUserFromSession } from "~/utils/auth.server";
 import HeaderAuth from "~/components/HeaderAuth";
+
+export function loader({ request }) {
+  const userId = getUserFromSession(request);
+
+  return userId;
+}
 
 export default function Authenticated() {
   return (
